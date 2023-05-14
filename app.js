53e870629df638568b8a05fe544f0087a2dc2bfd
Anderson Rosa <andyliqui@gmail.com>
2023-05-14 14:34:08 -0400

    running express

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require("express");
 
 const app = express();
 
-http.createServer(function(req,res){
-  res.write("mi islard")
-  res.end()	
+app.get("/",(re,res)=>{
+  res.send("mi islard from express")
+})
+// http.createServer(function(req,res){
+//   res.write("mi islard")
+//   res.end()	
 
-}).listen(3000)
+// }).listen(3000)
+app.listen(3000,()=>{
+  console.log("running server")
+})
